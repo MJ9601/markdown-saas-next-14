a7f6e40f88d6d702bd9918c37212e064c1e4af81
mohammadjavad khodadadi <m_javad_96@yahoo.com>
2023-12-14 15:09:19 +0330

    feat(popover): userinfo dispaly

--- src/components/ui/navbar/profile/index.tsx
+++ src/components/ui/navbar/profile/index.tsx
@@ -8,7 +8,10 @@ import {
   PopoverContent,
   PopoverTrigger,
   Popover,
+  Button,
 } from "../../general";
+import { MdDashboard, MdLogout } from "react-icons/md";
+import Link from "next/link";
 
 export default function Profile() {
   const { logged } = useAuth();
@@ -17,14 +20,44 @@ export default function Profile() {
     <div>
       <Popover>
         <PopoverTrigger>
-          <Avatar>
+          <Avatar className="ring-2 ring-primary ">
             <AvatarImage src={logged ? logged.image : ""} />
             <AvatarFallback>
               {logged && (logged.name![0] ?? logged.email[0])}
             </AvatarFallback>
           </Avatar>
         </PopoverTrigger>
-        <PopoverContent>some content here</PopoverContent>
+        <PopoverContent>
+          <div className="">
+            <p className="text-xl capitalize">
+              {logged ? logged.name! : "jane"}
+            </p>
+            <p className="text-xs mt-1 text-slate-400">
+              {logged ? logged.email : "jane@gmail.ocm"}
+            </p>
+            <hr className="mt-2" />
+            <div className="flex flex-col gap-1 mt-2">
+              <Link href="/dashboard">
+                <Button
+                  variant="ghost"
+                  className="w-full flex justify-between text-slate-300"
+                >
+                  <span className="pr-2">Dashboard</span>
+                  <MdDashboard size={20} />
+                </Button>
+              </Link>
+              <form action="">
+                <Button
+                  variant="ghost"
+                  className="w-full flex justify-between text-slate-300"
+                >
+                  <span className="pr-2">Logout</span>
+                  <MdLogout size={20} />
+                </Button>
+              </form>
+            </div>
+          </div>
+        </PopoverContent>
       </Popover>
     </div>
   );
